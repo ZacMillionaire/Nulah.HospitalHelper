61537296a5bb8ff06f9174ce414bb9d4783cb878
Scott <home@jotunga.com>
2022-01-02 15:34:57 +1000

    Added user repository classes and interfaces

---
+++ Nulah.HospitalHelper.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulah.HospitalHelper.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Creates a user for the given employee
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns>True on success, false if a user already exists for that employee</returns>
+        bool CreateUser(int employeeId);
+        /// <summary>
+        /// Logs a user in with the given password
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <param name="password"></param>
+        /// <returns>False on incorrect employeeId or password</returns>
+        bool Login(int employeeId, string password);
+        /// <summary>
+        /// Logs a user out by employeeId
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
+        bool Logout(int employeeId);
+    }
+}
---
+++ Nulah.HospitalHelper.Core/Models/Data/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulah.HospitalHelper.Core.Models.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string Salt { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
